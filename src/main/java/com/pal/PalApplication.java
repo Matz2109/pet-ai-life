260c2b4f6adda586206cf72062564a721778c5a7
Matz <matteo.sbicego96@gmail.com>
2026-02-05 17:58:48 -0500

    Add Lombok dependency and update Spring Boot application for JPA auditing

--- src/main/java/com/pal/PalApplication.java
+++ src/main/java/com/pal/PalApplication.java
@@ -2,8 +2,10 @@ package com.pal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PalApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/pal/PalApplicationTests.java
+++ src/test/java/com/pal/PalApplicationTests.java
@@ -2,8 +2,10 @@ package com.pal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class PalApplicationTests {
 
 	@Test
